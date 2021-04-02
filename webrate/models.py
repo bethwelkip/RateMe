eadd6a33b874bee564928b048aea25fd5275de65
Bethwel Kiplimo <bkiplimo@princeton.edu>
2021-04-02 07:33:49 -0400

    add views and modify models

--- webrate/models.py
+++ webrate/models.py
@@ -1,26 +1,30 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 # Create your models here.
+class Contact(models.Model):
+    Email = models.EmailField(max_length = 100)
+    Phone = models.CharField(max_length = 100)
+    Facebook = models.CharField(max_length = 100, blank = True, null = True)
+    Twitter = models.CharField(max_length = 100, blank = True, null = True)
+    LinkedIn = models.CharField(max_length = 100, blank = True, null = True)
+
 class User(models.Model):
     Name = models.CharField(max_length=100)
     Picture = CloudinaryField('image', null = True, blank = True)
-    Projects = models.ManyToManyField()
     Contact = models.ForeignKey(Contact, on_delete = models.DO_NOTHING)
-class Contact(models.Model):
-    Email = models.EmailField()
-    Phone = models.CharField()
-    Facebook = models.CharField(blank = True, null = True)
-    Twitter = models.CharField(blank = True, null = True)
-    LinkedIn = models.CharField(blank = True, null = True)
-class Project(models.Model):
-    Title = models.CharField(max_length = 100)
-    Image = models.ImageField()
-    Description = models.TextField(editable=True, blank = False)
-    url = models.CharField(blank = False, null = False)
-    ratings = Models.ManyToManyField(Ratings)
+
 class Ratings(models.Model):
     design = models.IntegerField()
     content = models.IntegerField()
     usability = models.IntegerField()
     overall = models.IntegerField()
     total_raters = models.IntegerField()
+
+class Project(models.Model):
+    Title = models.CharField(max_length = 100)
+    Image = CloudinaryField('image')
+    Description = models.TextField(editable=True, blank = False)
+    url = models.CharField(max_length = 100, blank = False, null = False)
+    ratings = models.ManyToManyField(Ratings)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
--- webrate/views.py
+++ webrate/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+def  register(request):
+    pass
+def login(request):
+    pass
+
+def home(request):
+    return render(request, 'html/base.html' )
+
+@login_required(login_url = 'auth/')
+def upload_project(request):
+    pass
+
+@login_required(login_url = 'auth/')
+def view_profile(request):
+    pass
+
+@login_required(login_url = 'auth/')
+def submit_rating(request):
+    pass
+    
+def search_project(request):
+    pass
+
 
 # Create your views here.
