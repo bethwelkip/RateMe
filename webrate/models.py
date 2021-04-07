b4018dcecae1591866e892220acf1e62e11456d5
Bethwel Kiplimo <bkiplimo@princeton.edu>
2021-04-07 09:41:08 -0400

    modify models

--- webrate/models.py
+++ webrate/models.py
@@ -2,18 +2,18 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 # Create your models here.
 class Contact(models.Model):
-    Email = models.EmailField(max_length = 100)
-    Phone = models.CharField(max_length = 100)
-    Facebook = models.CharField(max_length = 100, blank = True, null = True)
-    Twitter = models.CharField(max_length = 100, blank = True, null = True)
-    LinkedIn = models.CharField(max_length = 100, blank = True, null = True)
+    email = models.EmailField(max_length = 100)
+    phone = models.CharField(max_length = 100,blank = True, null = True)
+    facebook = models.CharField(max_length = 100, blank = True, null = True)
+    twitter = models.CharField(max_length = 100, blank = True, null = True)
+    linkedIn = models.CharField(max_length = 100, blank = True, null = True)
 
-class User(models.Model):
-    Name = models.CharField(max_length=100)
-    Picture = CloudinaryField('image', null = True, blank = True)
-    Contact = models.ForeignKey(Contact, on_delete = models.DO_NOTHING)
+class Users(models.Model):
+    name = models.CharField(max_length=100)
+    picture = CloudinaryField('image', null = True, blank = True)
+    contact = models.ForeignKey(Contact, on_delete = models.DO_NOTHING)
 
-class Ratings(models.Model):
+class Rating(models.Model):
     design = models.IntegerField()
     content = models.IntegerField()
     usability = models.IntegerField()
@@ -21,10 +21,10 @@ class Ratings(models.Model):
     total_raters = models.IntegerField()
 
 class Project(models.Model):
-    Title = models.CharField(max_length = 100)
-    Image = CloudinaryField('image')
-    Description = models.TextField(editable=True, blank = False)
+    title = models.CharField(max_length = 100)
+    image = CloudinaryField('image')
+    description = models.TextField(editable=True, blank = False)
     url = models.CharField(max_length = 100, blank = False, null = False)
-    ratings = models.ManyToManyField(Ratings)
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    ratings = models.ManyToManyField(Rating)
+    user = models.ForeignKey(Users, on_delete = models.CASCADE)
 
