18f0018bdd011e2af7d3e5ae49128db22bf9bb3b
Bethwel Kiplimo <bkiplimo@princeton.edu>
2021-04-08 04:45:30 -0400

    edit model relations

--- webrate/models.py
+++ webrate/models.py
@@ -10,21 +10,22 @@ class Contact(models.Model):
 
 class Users(models.Model):
     name = models.CharField(max_length=100)
+    password = models.CharField(max_length = 1000,blank = True)
     picture = CloudinaryField('image', null = True, blank = True)
     contact = models.ForeignKey(Contact, on_delete = models.DO_NOTHING)
 
 class Rating(models.Model):
-    design = models.IntegerField()
-    content = models.IntegerField()
-    usability = models.IntegerField()
-    overall = models.IntegerField()
-    total_raters = models.IntegerField()
+    design = models.IntegerField(blank=True)
+    content = models.IntegerField(blank = True)
+    usability = models.IntegerField(blank = True)
+    overall = models.FloatField(blank = True)
+    total_raters = models.IntegerField(blank = True)
 
 class Project(models.Model):
     title = models.CharField(max_length = 100)
-    image = CloudinaryField('image')
+    image = CloudinaryField('image', null = True, blank = True)
     description = models.TextField(editable=True, blank = False)
     url = models.CharField(max_length = 100, blank = False, null = False)
-    ratings = models.ManyToManyField(Rating)
-    user = models.ForeignKey(Users, on_delete = models.CASCADE)
+    ratings = models.ForeignKey(Rating, on_delete = models.CASCADE)
+    user = models.ForeignKey(Users,on_delete = models.CASCADE)
 
