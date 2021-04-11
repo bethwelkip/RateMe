b42058cd5dd4416fc2997c11030e87adab5200cd
Bethwel Kiplimo <bkiplimo@princeton.edu>
2021-04-11 17:20:26 -0400

    add view to view specific project

--- webrate/views.py
+++ webrate/views.py
@@ -7,46 +7,52 @@ from django.contrib.auth.hashers import check_password, make_password
 from django.contrib import messages
 from django_registration.forms import RegistrationForm
 from .models import Users, Project, Rating, Contact
-from .forms import LoginForm, UploadForm
+from .forms import LoginForm, UploadForm, RatingForm
 from .accounts import sample
 import cloudinary, cloudinary.api,cloudinary.uploader
+from werkzeug.security import generate_password_hash,check_password_hash
 
 def register(request):
-    form = RegistrationForm()
+    form = RegistrationForm(request.POST or None)
     if request.method == "POST":
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        user = User.objects.create_user(username=username, email = email, password = password)
-        user.save()
-        contact = Contact(email = email)
-        contact.save()
-        new_user = Users(name = username, password=make_password(password))
-        new_user.contact = contact
-        new_user.save()
-        return redirect('login')
-        #self.cursor.
-
+        if form.is_valid():
+            user= form.cleaned_data.get('username')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            user = User.objects.create_user(username=user, email=email, password=password)
+            print(generate_password_hash(password))
+            contact = Contact(email = email)
+            contact.save()
+            new_user = Users(name = user, password=generate_password_hash(password))
+            new_user.contact = contact
+            new_user.save()
+            return redirect('login')
+   
     return render(request, 'auth/registration_form.html', {"form": form})
 
 def login(request):
     form = AuthenticationForm()
     context = {"form":form}
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST.get('username')
+        password = request.POST.get('password')
         attempt_user = Users.objects.filter(name = username).first()
-        user = check_password(password, attempt_user.password) if attempt_user is not None else False
-        print(make_password(password), attempt_user.password)
-        # user = authenticate(request, username=username, password=check_password(password))
+        print(generate_password_hash(password))
+        print(check_password_hash(attempt_user.password, password))
+        print(attempt_user.password)
+        # attempt_user = Users.objects.filter(name = username).first()
+        # user = check_password(password, attempt_user.password) if attempt_user is not None else False
+        # print(make_password(password), attempt_user.password)
+        user = authenticate(request, username=username, password=password)
+        print(user)
         if user:
             log_in(request, user)
             return redirect('view_profile')
         else: 
             messages.info(request, "Username or Password is incorrect")
             context.update({"messages": messages})
+            # address4
             return redirect('login')
-
     return render(request, 'auth/login_form.html', context)
 
 def logout_me(request):
@@ -92,22 +98,49 @@ def view_profile(request):
     return render(request, 'html/profile.html')
 
 # @login_required(login_url = 'auth/')
-def submit_rating(request):
-    pass
+def submit_rating(request, id):
+    ied = id
+    state = True
+    form = RatingForm()
+    rated_project = Project.objects.filter(id = int(ied)).first()
+    if form.is_valid() or request.method == 'POST':
+        usab = int(request.POST['usability'])
+        cont = int(request.POST['content'])
+        des = int(request.POST['design'])
+        current_usab = (rated_project.ratings.usability)*(rated_project.ratings.total_raters) + usab
+        current_cont = rated_project.ratings.content*(rated_project.ratings.total_raters) + cont
+        current_des = rated_project.ratings.design*(rated_project.ratings.total_raters) + des
+        total_raters = rated_project.ratings.total_raters + 1
+        rated_project.ratings.total_raters = total_raters
+        rated_project.ratings.design = round(current_des/total_raters, 2)
+        rated_project.ratings.content =  round(current_cont/total_raters, 2)
+        rated_project.ratings.usability = round(current_usab/total_raters, 2)
+        rated_project.ratings.overall = round((rated_project.ratings.design + rated_project.ratings.content  + rated_project.ratings.usability )/(3), 2)
+        rated_project.ratings.save()
+        rated_project.save()
+        project = [project for project in Project.objects.all()]
+        projects = [(project, [project.ratings],True if project.id == int(ied) else False) for project in project] 
+        return redirect('view_project', id = ied)
+    project = [project for project in Project.objects.all()]
+    projects = [(project, [project.ratings],True if project.id == int(ied) else False) for project in project] 
+    return render(request, 'html/image.html',{"state": state,"form":form, "project": rated_project})
     
 def search_project(request):
     if 'project' in request.GET and request.GET["project"]:
         search_term = request.GET.get("project")
         searched_projects = Project.search_projects(search_term)
         projects = list()
-        for user in searched_projects:
-            projects.append(Project.objects.filter(project__id = user.id).first())
-        message = f"{search_term}"
+        for project in searched_projects:
+            projects.append((project, [project.ratings], project.id))
+        message = f"You have searched for {search_term}"
         return render(request, 'html/home.html',{"message":message,"projects": projects})
 
     else:
-        message = "You haven't searched for any term \n"
-        return render(request, 'search.html',{"message":message})
+        return redirect('home')
+def view_project(request, id):
+    rated_project = Project.objects.filter(id = int(id)).first()
+    state = False
+    return render(request, 'html/image.html', {"project": rated_project,"button": state})
 
 
 # Create your views here.
