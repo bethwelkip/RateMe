7ffa645b55ceeee552123564ef86f0b638719cc8
Bethwel Kiplimo <bkiplimo@princeton.edu>
2021-04-11 17:18:22 -0400

    edit forms for rating projects

--- webrate/forms.py
+++ webrate/forms.py
@@ -17,3 +17,19 @@ class UpdateForm(forms.ModelForm):
     class Meta:
         model = Users
         exclude = []
+class RatingForm(forms.Form):
+    choices = (
+    ("1", 1),
+    ("2", 2),
+    ("3",3),
+    ("4", 4),
+    ("5", 5),
+    ("6", 6),
+    ("7", 7),
+    ("8", 8),
+    ("9", 9),
+    ("10", 10),
+)
+    design = forms.ChoiceField(choices=choices)
+    content = forms.ChoiceField(choices=choices)
+    usability = forms.ChoiceField(choices=choices)
